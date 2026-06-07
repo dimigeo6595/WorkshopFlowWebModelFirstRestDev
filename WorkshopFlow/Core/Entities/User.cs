aaddeea137625c6ea5c2c88b0b3505768e816017
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-06-07 19:34:32 +0300

    feat: add authentication domain entities

---
+++ WorkshopFlow/Core/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using WorkshopFlow.Core.Entities;
+
+namespace WorkshopFlow.Core.Entities;
+
+public class User : BaseEntity
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public int RoleId { get; set; }
+
+    public Role Role { get; set; } = null!;
+
+}
